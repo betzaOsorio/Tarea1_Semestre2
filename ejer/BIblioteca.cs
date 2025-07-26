30e244970e9ff710a2a45685928f467029ba4d47
Betza <betza.otaku.karla@gmail.com>
2025-07-26 17:18:28 -0600

    Ejercicio1

---
+++ ejer/BIblioteca.cs
@@ -0,0 +1,52 @@
+﻿using ejer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ejer
+{
+    public class Biblioteca
+    {
+        private List<Libro> libros;
+        private int contadorId;
+
+        public Biblioteca()
+        {
+            libros = new List<Libro>();
+            contadorId = 1;
+        }
+
+        public bool RegistrarLibro(string titulo, string autor)
+        {
+            if (libros.Any(l => l.Titulo.ToLower() == titulo.ToLower()))
+                return false;
+
+            Libro nuevoLibro = new Libro(titulo, autor, contadorId++);
+            libros.Add(nuevoLibro);
+            return true;
+        }
+
+        public bool PrestarLibro(string titulo, string usuario)
+        {
+            Libro libro = libros.FirstOrDefault(l => l.Titulo.ToLower() == titulo.ToLower() && l.Disponible);
+
+            if (libro == null)
+                return false;
+
+            return libro.Prestar(usuario);
+        }
+
+        public List<string> ObtenerLibrosDisponibles()
+        {
+            return libros
+                .Where(l => l.Disponible)
+                .Select(l => l.ToString())
+                .ToList();
+        }
+    }
+}
+
+
--- ejer/Program.cs
+++ ejer/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ejer;
+
+namespace ejer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca miBiblioteca = new Biblioteca();
+
+            
+            miBiblioteca.RegistrarLibro("Antes de Diciembre", "Joana Marcus");
+            miBiblioteca.RegistrarLibro("Twisted Love", "Ana Huang");
+            miBiblioteca.RegistrarLibro("Twisted Games", "Ana Huang");
+
+            
+            if (!miBiblioteca.RegistrarLibro("Twisted Love", "Ana Huang"))
+            {
+                Console.WriteLine("El libro 'Twisted Love' ya está registrado actualmente.");
+            }
+
+            
+            bool prestado = miBiblioteca.PrestarLibro("Twisted Love", "Betza");
+            Console.WriteLine(prestado ? "Préstamo exitoso." : "El libro no está disponible en este momento.");
+
+            
+            Console.WriteLine("\nLibros disponibles:");
+            foreach (string info in miBiblioteca.ObtenerLibrosDisponibles())
+            {
+                Console.WriteLine(info);
+            }
+        }
+    }
+}
