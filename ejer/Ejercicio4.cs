dbc91ac48c14f5cc8b02f5d995fe54ead5e4b24f
Jennifer-dg <dgaby0214@gmail.com>
2025-07-26 18:31:56 -0600

    Ejercicio 4

---
+++ ejer/Ejercicio4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    internal class Ejercicio4
+    {
+        public abstract class Vehiculo
+        {
+            public void Encender()
+            {
+                Console.WriteLine("El vehiculo está encendido");
+            }
+            public abstract void Conducir();
+        }
+        public class Carro : Vehiculo
+        {
+            public override void Conducir()
+            {
+                Console.WriteLine("Conduciendo el carro");
+            }
+        }
+        public class Motocicleta : Vehiculo
+        {
+            public override void Conducir()
+            {
+                Console.WriteLine("Conduciendo la motocicleta");
+            }
+        }
+    }
+}
---
+++ ejer/Program4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using static ejercicio4.Ejercicio4;
+
+class Program
+{
+    static void Main()
+    {
+        List<Vehiculo> vehiculo = new List<Vehiculo>
+        {
+            new Carro(),
+            new Motocicleta()
+        };
+        foreach (Vehiculo v in vehiculo)
+        {
+            v.Encender();
+            v.Conducir();
+            Console.WriteLine();
+        }
+    }
+}
